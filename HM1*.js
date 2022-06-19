a4ca3f0b3846196608b008c140b5702745a48b02
shardinsv <sergey.shardin@gmail.com>
2022-06-19 19:42:12 +0500

    Change JSHM1

--- HM1*.js
+++ HM1*.js
@@ -1,16 +1,29 @@
 let age_2 = 18, age_3 = 60;
 const Fech = function(age){
+// if (age && !isNaN(age)) {
+//     console.log(age)
+// if (age < age_2){
+//      console.log("You don’t have access cause your age is " + age + " It’s less than " + age_2)
+// } else if(age >=  age_2&&age < age_3){
+//     console.log("Welcome !")
+// } else if(age > age_3){
+//     console.log("Keep calm and look Culture channel")
+// } 
+// else{console.log("Technical Work")}
+// } else {console.log("NaN")}
+// }
+
 if (age && !isNaN(age)) {
-    console.log(age)
+    alert(age)
 if (age < age_2){
-     console.log("You don’t have access cause your age is " + age + " It’s less than " + age_2)
+    alert("You don’t have access cause your age is " + age + " It’s less than " + age_2)
 } else if(age >=  age_2&&age < age_3){
-    console.log("Welcome !")
+    alert("Welcome !")
 } else if(age > age_3){
-    console.log("Keep calm and look Culture channel")
+    alert("Keep calm and look Culture channel")
 } 
-else{console.log("Technical Work")}
-} else {console.log("NaN")}
+else{alert("Technical Work")}
+} else {alert("NaN")}
 }
 // Fech(17)
 // Fech(18)
