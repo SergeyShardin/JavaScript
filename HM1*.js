93bfbd84ca092f3bcec0f763d9271ba2bac064a3
shardinsv <sergey.shardin@gmail.com>
2022-06-19 19:22:47 +0500

    create HM*JS

---
+++ HM1*.js
@@ -0,0 +1,25 @@
+let age_2 = 18, age_3 = 60;
+const Fech = function(age){
+if (age && !isNaN(age)) {
+    console.log(age)
+if (age < age_2){
+     console.log("You don’t have access cause your age is " + age + " It’s less than " + age_2)
+} else if(age >=  age_2&&age < age_3){
+    console.log("Welcome !")
+} else if(age > age_3){
+    console.log("Keep calm and look Culture channel")
+} 
+else{console.log("Technical Work")}
+} else {console.log("NaN")}
+}
+// Fech(17)
+// Fech(18)
+// Fech(61)
+// Fech(60)
+// Fech('fich')
+// Fech('')
+// Fech(0)
+// Fech(NaN)
+
+let TestBrowser = prompt('your values')
+Fech(TestBrowser)
