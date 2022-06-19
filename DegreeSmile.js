d1f332519434f41639429bf5436f078f16e79405
shardinsv <sergey.shardin@gmail.com>
2022-06-20 01:59:46 +0500

    Create DegreeSmile.js

--- DegreeSmile.js
+++ DegreeSmile.js
@@ -1,16 +1,12 @@
-// let b = 2
-// for(i = 1; i < 10; i++)
-// console.log(b = b * 2)
+let b = 1
+for(i = 0; i < 10; i++)
+console.log(b = b * 2)
 
 const Stepen = function(chislo){
-    if(chislo > 1 && chislo <= 10){
-        let b = 2
-        for(i = 1; i < chislo; i++)
+    if (chislo <= 10){
+        let b = 1
+        for(i = 0; i < chislo; i++)
 console.log(b = b * 2)
     }
 }
 Stepen(10)
-
-// let chislo = 1
-// let stepen = 10
-// let result = 2
