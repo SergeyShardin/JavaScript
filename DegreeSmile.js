e3bf619ed962f879ee7fceefa21998d2819bb1a0
shardinsv <sergey.shardin@gmail.com>
2022-06-21 00:21:54 +0500

    changed HM2*_JavaScript

--- DegreeSmile.js
+++ DegreeSmile.js
@@ -1,12 +1,42 @@
-let b = 1
-for(i = 0; i < 10; i++)
-console.log(b = b * 2)
+// let ST = 1
+// for(i = 0; i < 10; i++)
+// console.log(ST = ST * 2)
 
-const Stepen = function(chislo){
-    if (chislo <= 10){
-        let b = 1
-        for(i = 0; i < chislo; i++)
-console.log(b = b * 2)
-    }
-}
-Stepen(10)
+// const Stepen = function(chislo){
+//     if (chislo <= 10){
+//         let b = 1
+//         for(i = 0; i < chislo; i++)
+// console.log(b = b * 2)
+//     }
+// }
+// Stepen(10)
+
+//        let b = [":)", ":)" + ":)", ':)' + ':)' + ':)', ':)' + ':)' + ':)' + ':)', ':)' + ':)' + ':)' + ':)' + ':)']
+//         for (let i = 0; i < 5; i++)
+// console.log(b[i])
+
+// let Smile = function(string, num_s){
+// let b = ''
+// let count = 0
+// for (i = 0; i < num_s; i++)
+// console.log(count++, b = b + string)}
+// Smile(':)', 5)
+
+// function vowel_count(string) {
+//     let vowel = 'aeiouAEIOU'
+//     let consonants = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'
+//     let vcount = 0
+//     let concount = 0
+    
+//     for(let x = 0; x < string.length; x++) {
+//       if (vowel.indexOf(string[x]) !== -1) {
+//         vcount++;
+//       } if (consonants.indexOf(string[x]) !== -1)
+//       concount++
+//     }
+//     console.log('Слово WORD состоит из ' + vcount + ' гласных и ' + concount + ' согласных букв')
+//   }
+//   vowel_count('case')
+//   vowel_count('Case')
+//   vowel_count('Check-list')
+
